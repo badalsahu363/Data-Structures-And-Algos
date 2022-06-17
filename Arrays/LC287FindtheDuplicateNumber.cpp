6996eab05bf2494af75d7c496db91fc277600428
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2022-06-17 12:26:43 +0530

    Create LC287FindtheDuplicateNumber.cpp

---
+++ Arrays/LC287FindtheDuplicateNumber.cpp
@@ -0,0 +1,22 @@
+linked list cycle methoid for o(n)tc and sc : o(1)
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
+            
+    }
+};
